3f9211f5f358bcd0bd31db638daecefdfe25455e
unknown <ee1210@naver.com>
2021-02-05 15:23:15 +0900

    sensor

---
+++ arduino_sensor/reedswitch/reedswitch.ino
@@ -0,0 +1,27 @@
+int read_pin = 13; // 다른스위치핀은 gnd
+int state;
+int pulse_pin = 5;
+int dir_pin = 4;
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(read_pin, INPUT_PULLUP);
+  pinMode(pulse_pin, OUTPUT);
+  pinMode(dir_pin, OUTPUT);
+  pinMode(7, OUTPUT);
+
+}
+
+void loop() {
+  state = digitalRead(read_pin);
+  
+  if (state == HIGH) {
+    Serial.println("open");
+
+    tone(pulse_pin, 1000);
+  }
+  else {
+    Serial.println("close");
+    noTone(pulse_pin);
+  }
+}
